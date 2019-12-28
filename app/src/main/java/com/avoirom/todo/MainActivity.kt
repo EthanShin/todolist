f09db13c8cb3904edaf1755185f0b25e1cf8cd15
avoirom <avoirom@avoiroms-MacBook.local>
2019-12-28 21:59:19 +0900

    feat: Add ListFragment
    description:
    1. Create ListFragment to show todo-list.
    2. Add FrameLayout in the activity_main.xml.
    3. Add ListFragment in MainActivity.

--- app/src/main/java/com/avoirom/todo/MainActivity.kt
+++ app/src/main/java/com/avoirom/todo/MainActivity.kt
@@ -2,11 +2,18 @@ package com.avoirom.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.avoirom.todo.ui.ListFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.listFragment, ListFragment.newInstance())
+                .commitNow()
+        }
     }
 }
